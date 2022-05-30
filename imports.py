a1635763ba8506e803d92bc042469b05abb7b9c4
Juuso Silvo <juusosilvo03@gmail.com>
2022-05-30 03:12:33 +0300

    User-Agent + cleaned code

--- imports.py
+++ imports.py
@@ -7,4 +7,3 @@ from os import path
 from requests.exceptions import Timeout
 from requests.exceptions import ConnectionError
 from requests.exceptions import HTTPError
-from requests.exceptions import SSLError
